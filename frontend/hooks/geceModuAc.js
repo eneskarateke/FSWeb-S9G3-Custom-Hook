1dd7d2f8c85391a8b916f9aecbce5e8cc16002c0
eneskarateke <eneskarateke@gmail.com>
2023-05-17 15:22:11 +0300

    usenightmode old version commented out

--- frontend/hooks/geceModuAc.js
+++ frontend/hooks/geceModuAc.js
@@ -1,4 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+// export const useNightMode = (initialValue) => {
+//   const [value, setValue] = useState(initialValue);
+//   const handleChanges = (updatedValue) => {
+//     setValue(updatedValue);
+//   };
+//   return [value, setValue, handleChanges];
+// };
+
 import { useLocalStorage } from "./localStorageKullan";
 
 export const useNightMode = (key, initialValue) => {
